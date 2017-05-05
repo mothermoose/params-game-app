6c206dcf45ed270bbdd0c11048777b51d7fbf24b
mothermoose <melanie.ilseman@gmail.com>
2017-05-04 21:26:37 -0500

    forms addition + exercise

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -21,4 +21,23 @@ def game
     @answer_message = "right on the money!!!"
   end 
  end 
+
+ def form_show
+
+ end 
+
+ def form_send
+  @user_guess = params["form_message"].to_i
+  winning_number = 42
+
+  if winning_number > @user_guess
+    @answer_message = "too high."
+  elsif winning_number < @user_guess
+    @answer_message = "too low."
+  elsif winning_number == @user_guess
+    @answer_message = "right on the money!!!"
+  end 
+  render 'form_show.html.erb'
+end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get '/name' => 'games#game'
   get '/guess/:this_is_a_key' => 'games#guess'
+
+  get '/form_show_params' => 'games#form_show'
+  post '/form_send_params' => 'games#form_send'
 end
